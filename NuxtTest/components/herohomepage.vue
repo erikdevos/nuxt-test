6a26be16d3255cf28c418b14c99cec5ed6c89626
erikdevos <erikdevos87@gmail.com>
2024-11-09 15:33:03 +0100

    Move homepage hero to component.

---
+++ NuxtTest/components/herohomepage.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="hero">
+        <img src="assets/resources/homepage-header.webp" alt="">
+        <div class="container">
+            <h1>Welcome to My Nuxt App</h1>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+    /* Homepage specific */
+    section.hero {
+        display: flex;
+        position: relative;
+
+        .container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: absolute;
+            height: 100%;
+            top: 0;
+            left: auto;
+            right: auto;
+        }
+        img {
+            object-fit: cover;
+            width: 100%;
+            max-height: 300px;
+            position: relative;
+        }
+
+        h1 {
+            position: absolute;
+            z-index: 2;
+        }
+    }
+</style>
--- NuxtTest/pages/index.vue
+++ NuxtTest/pages/index.vue
@@ -1,11 +1,5 @@
 <template>
-    
-    <section class="hero">
-        <img src="assets/resources/homepage-header.webp" alt="">
-        <div class="container">
-            <h1>Welcome to My Nuxt App</h1>
-        </div>
-    </section>
+    <Herohomepage />
 
     <section>
         <div class="container">
@@ -16,32 +10,3 @@
     </section>
 
 </template>
-
-<style scoped lang="scss">
-    /* Homepage specific */
-    section.hero {
-        display: flex;
-        position: relative;
-
-        .container {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            position: absolute;
-            top: 0;
-            left: auto;
-            right: auto;
-        }
-        img {
-            object-fit: cover;
-            width: 100%;
-            max-height: 300px;
-            position: relative;
-        }
-
-        h1 {
-            position: absolute;
-            z-index: 2;
-        }
-    }
-</style>
