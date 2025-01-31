5017d5084d7b0710a5bde364037b119880ce87f5
erikdevos <erikdevos87@gmail.com>
2025-01-31 10:10:47 +0100

    Update readme. Update contact page.

--- NuxtTest/pages/contact.vue
+++ NuxtTest/pages/contact.vue
@@ -1,32 +1,42 @@
 <template>
     <div class="page-content contact-page">
-      <h1>Contact Us</h1>
+      <h1>Contact us</h1>
+      <p class="contact-intro">We love to hear from you! Questions, compliments, offers? Let us know.</p>
+
       <form @submit.prevent="submitForm">
         <!-- Name Field -->
-        <label for="name">Name:</label>
-        <input type="text" id="name" v-model="formData.name" required />
+        <div class="input-row">
+          <label for="name">Name:</label>
+            <input type="text" id="name" v-model="formData.name" required />
+        </div>
   
         <!-- Email Field -->
-        <label for="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          v-model="formData.email"
-          required
-          placeholder="example@example.com"
-        />
+        <div class="input-row">
+          <label for="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              v-model="formData.email"
+              required
+              placeholder="example@example.com"
+            />
+      </div>
   
         <!-- Message Field -->
-        <label for="message">Message:</label>
-        <textarea
-          id="message"
-          v-model="formData.message"
-          rows="4"
-          required
-        ></textarea>
-  
+         <div class="input-row">
+          <label for="message">Message:</label>
+          <textarea
+            id="message"
+            v-model="formData.message"
+            rows="4"
+            required
+          ></textarea>
+        </div>
+
         <!-- Submit Button -->
-        <button type="submit">Send Message</button>
+         <div class="button-row">
+          <button type="submit">Send Message</button>
+        </div>
       </form>
     </div>
   </template>
@@ -53,6 +63,28 @@
     max-width: 600px;
     margin: 0 auto;
     padding: 20px;
+    form {
+      .button-row {
+        display: flex;
+        justify-content: flex-end;
+      }
+      button[type=submit] {
+        align-self: flex-end;
+      }
+    }
+
+    .input-row {
+      display: flex;
+      margin-bottom: 1rem;
+      label {
+        flex: 1;
+      }
+      input,
+      textarea,
+      select {
+        flex: 3;
+      }
+    }
   }
   </style>
   
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Simple Nuxt project with Pinia.
+
+Start with NPM run dev
